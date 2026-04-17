73d034aa64caaba1521290924514b8e77fce110a
aminedevaj <developerwebaj@gmail.com>
2026-04-17 17:13:14 +0100

    Fix: correct runtime version3

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+
+// 1. Cree l-fichie sqlite f /tmp ila makanch
+$dbPath = '/tmp/database.sqlite';
+if (!file_exists($dbPath)) {
+    touch($dbPath);
+}
+
+// 2. Load Laravel
 require __DIR__ . '/../public/index.php';
