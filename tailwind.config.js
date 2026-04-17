1d5a41be78a0157eb499fa980fdddf54ef22f1ad
aminedevaj <developerwebaj@gmail.com>
2026-04-17 18:42:35 +0100

    fix1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Hna t-zid l-alwan dyal l-brand li mkhddam f l-code
+        'brand-blue': '#2563eb', // Example: baddalha b l-blue dyalk
+        'brand-black': '#0a0a0a',
+        'brand-dark': '#1a1a1a',
+      },
+      fontFamily: {
+        // Hna khass t-zid l-font 'DM Serif Display' bach t-ban f l-build
+        'serif': ['"DM Serif Display"', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
