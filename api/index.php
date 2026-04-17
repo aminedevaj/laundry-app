37f3e5d39324dc61c563030d2fb4de064d64abf9
aminedevaj <developerwebaj@gmail.com>
2026-04-17 17:36:12 +0100

    Fix: correct runtime version6

--- api/index.php
+++ api/index.php
@@ -1,10 +1,14 @@
 <?php
 
-// 1. Cree l-fichie sqlite f /tmp ila makanch
+// Cree l-folders darouriyen f /tmp
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0777, true);
+}
+
+// L-base de données
 $dbPath = '/tmp/database.sqlite';
 if (!file_exists($dbPath)) {
     touch($dbPath);
 }
 
-// 2. Load Laravel
 require __DIR__ . '/../public/index.php';
